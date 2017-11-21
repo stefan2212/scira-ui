4bda9f3930e6e9d43f90bfc51c19682da06446af
utilizatorvalid <vasilegroza3@gmail.com>
2017-11-21 17:48:27 +0200

    auth guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 
 import { DataService } from './services/data.service';
+import { UserService } from './services/user.service';
 import { AboutComponent } from './components/about/about.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component'
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
-import { MaterializeModule } from "angular2-materialize";
+import { MaterializeModule } from 'angular2-materialize';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { AuthGuard } from './guards/auth.guard';
+import { AuthService } from './services/auth.service';
 
 
 
 const appRoutes: Routes = [
-  {path: '', component:UserComponent},
-  {path: 'search', component:AboutComponent}
-]
+  { path: '', component: UserComponent, canActivate: [AuthGuard] },
+  { path: 'search', component: AboutComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
+];
 
 @NgModule({
   declarations: [
@@ -28,6 +35,8 @@ const appRoutes: Routes = [
     AboutComponent,
     NavBarComponent,
     SideBarComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
@@ -35,7 +44,7 @@ const appRoutes: Routes = [
     HttpModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [DataService],
+  providers: [DataService, UserService, AuthGuard, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
